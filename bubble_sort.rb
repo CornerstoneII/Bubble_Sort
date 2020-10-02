4b3181da4a9e07451b364f33bf1900cc9771aa5c
Stephan Annandale <sa070197@gmail.com>
2020-10-02 12:20:52 +0200

    Fix rubocop errors #2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,21 @@
+# Start method called bubble_sort
 def bubble_sort(array)
+  # Amount of times bubble_sort is iterated (arr_length)
   arr_length = array.length
 
   loop do
+    # Ender ends loop when (see break unless = line 17)
     ender = false
-
+    # Switch cases and set ender = true
     (arr_length - 1).times do |n|
-      if array[n] > array[n + 1]
-        array[n], array[n + 1] = array[n + 1], array[n]
+      if array[n] > array[n.next]
+        array[n], array[n.next] = array[n.next], array[n]
         ender = true
       end
     end
 
     break unless ender
   end
-
+  # Return array
   array
 end
