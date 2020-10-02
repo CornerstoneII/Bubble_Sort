43e31d5c567925d6f8bd426ba4637f774f5ad709
Stephan Annandale <sa070197@gmail.com>
2020-10-02 12:10:38 +0200

    Fix robocop errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,18 @@
-def bubble_sort (array)
-
-  arr_length = array.arr_length
+def bubble_sort(array)
+  arr_length = array.length
 
   loop do
+    ender = false
 
-  ender = false
-
-  (arr_length -1).times do |n|
-    if array[n] > array[n + 1]
-      array[n], array[n + 1] = array[n + 1], array[n]
-      ender = true
+    (arr_length - 1).times do |n|
+      if array[n] > array[n + 1]
+        array[n], array[n + 1] = array[n + 1], array[n]
+        ender = true
+      end
     end
+
+    break unless ender
   end
 
-  break if not ender
-end
-array
+  array
 end
