d768f8e76f5d3e1363c07cbae13323914ce04637
CornerstoneII <seunmyles@gmail.com>
2020-10-02 10:34:45 +0100

    Fix  bubble sort loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,3 +3,17 @@ def bubble_sort (array)
   arr_length = array.arr_length
 
   loop do
+
+  ender = false
+
+  (arr_length -1).times do |n|
+    if array[n] > array[n + 1]
+      array[n], array[n + 1] = array[n + 1], array[n]
+      ender = true
+    end
+  end
+
+  break if not ender
+end
+array
+end
