ff001b080238d2e0d9d22d53372290fbef97aff5
Stephan Annandale <sa070197@gmail.com>
2020-10-05 11:53:13 +0200

    Modify bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,3 +19,28 @@ def bubble_sort(array)
   # Return array
   array
 end
+
+# bubble_sort_by
+
+def bubble_sort_by(array)
+  # Amount of times bubble_sort is iterated (arr_length)
+  arr_length = array.length
+
+  # use yield method
+  (arr_length - 1).times do
+    (0..(arr_length - 2)).each do |n| # each aray item iterated
+      ender = yield array[n], array[n.next]
+      array[n], array[n.next] = array[n.next], array[n] if ender.positive?
+    end
+  end
+  # Return array
+  array
+end
+
+a = ["Yo mah G", "hi", "hello", "ya"]
+
+switch_result = bubble_sort_by(a) do |left, right|
+  left.length - right.length
+end
+
+puts switch_result
